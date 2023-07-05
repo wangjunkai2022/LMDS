e2c0f1613b9f496e391fdcecfe70b0f62747b135
xiaokai <xiaokai@kk.com>
2023-07-05 16:56:33 +0700

    执行时间修改

--- add-ping-2sys.sh
+++ add-ping-2sys.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo mv ./auto_ping.sh /bin/yy_auto_ping.sh
 sudo chmod 777 /bin/yy_auto_ping.sh
-sudo crontab -l >conf && echo "* * * * * yy_auto_ping.sh" >>conf && crontab conf && rm -f conf
+sudo crontab -l >conf && echo "*/1 * * * * yy_auto_ping.sh" >>conf && sudo crontab conf && sudo rm -f conf
