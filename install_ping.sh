2c94cebcb5f2e2c71e0ae786e57a1f14a9b4543c
xiaokai <xiaokai@kk.com>
2023-07-05 17:02:16 +0700

    改名为install并且执行后删除文件

--- install_ping.sh
+++ install_ping.sh
@@ -2,3 +2,4 @@
 sudo mv ./auto_ping.sh /bin/yy_auto_ping.sh
 sudo chmod 777 /bin/yy_auto_ping.sh
 sudo crontab -l >conf && echo "*/1 * * * * yy_auto_ping.sh" >>conf && sudo crontab conf && sudo rm -f conf
+sudo rm -rf ./install_ping.sh
